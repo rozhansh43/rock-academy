1a0d39771dd83954f08656d6e8d311d80dc7ca19
Khatabakhsh <khatabakhsha@gmail.com>
2025-09-23 00:25:11 +0330

    add header of classes page

--- app/classes/page.tsx
+++ app/classes/page.tsx
@@ -1,3 +1,27 @@
+import { Button } from '@/components/ui/button';
+import { Input, InputWrapper } from '@/components/ui/input';
+import { MenuIcon, SearchIcon } from 'lucide-react';
+import { ProfileIcon } from '@/components/icons/profile-icon';
+
 export default function Page() {
-  return <></>;
+  return (
+    <div className="container-main pt-8">
+      <div className="flex flex-row items-center justify-between gap-2.5">
+        <InputWrapper variant="lg">
+          <SearchIcon />
+          <Input
+            variant="lg"
+            placeholder="نام کلاس مورد نظر خود را جستجو کنید."
+          />
+        </InputWrapper>
+
+        <Button variant="dim" mode="icon">
+          <ProfileIcon className="size-6 stroke-zinc-500" />
+        </Button>
+        <Button variant="dim" mode="icon">
+          <MenuIcon className="size-6 stroke-zinc-500" />
+        </Button>
+      </div>
+    </div>
+  );
 }
