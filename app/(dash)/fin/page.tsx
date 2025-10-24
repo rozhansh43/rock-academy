19bae285f54a328bba45eb2da456d2aed6c16780
Khatabakhsh <khatabakhsha@gmail.com>
2025-10-24 15:44:37 +0330

    create fin page

--- app/(dash)/fin/page.tsx
+++ app/(dash)/fin/page.tsx
@@ -1,5 +1,44 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { cn } from '@/utils/cn';
+import { useState } from 'react';
+
 export default function Page() {
-  return <div className="">مالی</div>;
+  const [activeTab, setActiveTab] = useState<'paid' | 'pend' | 'past'>('paid');
+
+  const tabs: { title: string; value: typeof activeTab }[] = [
+    { title: 'پرداخت شده', value: 'paid' },
+    { title: 'در انتظار', value: 'pend' },
+    { title: 'از موعد گذشته', value: 'past' },
+  ];
+
+  return (
+    <div className="container-main size-full pt-6">
+      <div className="flex flex-row justify-between">
+        {tabs.map((item) => (
+          <div key={item.value} className="flex flex-col items-center gap-0.5">
+            <Button
+              onClick={() => setActiveTab(item.value)}
+              variant="dim"
+              className={cn(
+                'text-middle-gray text-base',
+                item.value === activeTab &&
+                  'text-primary hover:text-primary/90',
+              )}
+            >
+              {item.title}
+            </Button>
+            <div
+              className={cn(
+                'bg-primary invisible size-2 rounded-full',
+                item.value === activeTab && 'visible',
+              )}
+            />
+          </div>
+        ))}
+      </div>
+      <div className=""></div>
+    </div>
+  );
 }
